7f7f5dcdb3ede682501d3cd05542a36f8c6067fd
mrdaveu <ulokwa@gmail.com>
2023-11-05 09:21:23 +0800

    antialias

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ document.addEventListener("DOMContentLoaded", function() {
          Object.keys(data).forEach(id => {
             const listItem = document.createElement("li");
             listItem.className = "data";
+            listItem.id = "data" + id;
             ['songName', 'artist', 'difficulty', 'instrument', 'tempo'].forEach(key => {
             const div = document.createElement("div");
             div.innerHTML = key === 'difficulty' ? ['V.Easy', 'Easy', 'Medium', 'Hard', 'V.Hard'][data[id][key] - 1] : data[id][key];
@@ -44,62 +45,68 @@ document.addEventListener("DOMContentLoaded", function() {
       });
 });
 
-   function updateContent(item, id) {
-      const xml_src = "https://raw.githubusercontent.com/mrdaveu/licksite/main/" + id + ".musicxml";
-      const audio_src = "https://raw.githubusercontent.com/mrdaveu/licksite/main/" + id + ".m4a";
-      const audio = new Audio(audio_src);
-      document.getElementById("osmdContainer").innerHTML = "";
-      document.getElementById("title").innerHTML = "";
-      const osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay("osmdContainer");
-      osmd.setOptions({
-         backend: "svg",
-         drawTitle: false,
-         drawMeasureNumbers: false, 
-         defaultFontFamily: 'Noto Serif',
-         drawingParameters: "compacttight"
-      });
-      osmd
-         .load(xml_src)
-         .then(() => {
-            if (window.innerWidth < 650) {
-               osmd.Zoom = 0.66; 
-            }
-            else {
-               osmd.Zoom = 0.75;
-            }
-            osmd.render();
-      });
-         const titleElement = document.createElement("h2");
-         const artistElement = document.createElement("div");
-         const subheadElement = document.createElement("h3");
-       
-         titleElement.innerHTML = item["songName"] || "";
-         artistElement.innerHTML = item["albumName"] ? `${item["artist"]} on "${item["albumName"]}"` : (item["artist"] || "");
-         subheadElement.innerHTML = 
-         (item["tempo"] ? `Tempo: ${item["tempo"]}` : "") + (item["difficulty"] ? ` / ${['Easiest', 'Easier', 'Easy', 'Not That Easy', 'Not Easy At All'][item["difficulty"] - 1]}` : "")
-         + (item["chordProgression"] ? ` / Chords: ${item["chordProgression"]}` : "");
-         document.getElementById("title").appendChild(titleElement);
-         document.getElementById("title").appendChild(artistElement);
-         document.getElementById("title").appendChild(subheadElement);
-
-         // Update description
-         const description = document.getElementById("description");
-         description.innerHTML = item["description"] ? `${item["description"]}` : "";
-         
-         function playAudio() {
-
-            if (activeAudio) {
-              activeAudio.pause();
-            }
-            audio.play();
-            activeAudio = audio;
+function updateContent(item, id) {
+   const xml_src = "https://raw.githubusercontent.com/mrdaveu/licksite/main/" + id + ".musicxml";
+   const audio_src = "https://raw.githubusercontent.com/mrdaveu/licksite/main/" + id + ".m4a";
+   const audio = new Audio(audio_src);
+   document.getElementById("osmdContainer").innerHTML = "";
+   document.getElementById("title").innerHTML = "";
+   const osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay("osmdContainer");
+   osmd.setOptions({
+      backend: "svg",
+      drawTitle: false,
+      drawMeasureNumbers: false, 
+      defaultFontFamily: 'Noto Serif',
+      drawingParameters: "compacttight"
+   });
+   osmd
+      .load(xml_src)
+      .then(() => {
+         if (window.innerWidth < 650) {
+            osmd.Zoom = 0.66; 
          }
-         playAudio();
-         const playButton = document.getElementById("playLick");
-         playButton.addEventListener("click", () => {
-         playAudio();
-         });
+         else {
+            osmd.Zoom = 0.75;
+         }
+         osmd.render();
+   });
+   const titleElement = document.createElement("h2");
+   const artistElement = document.createElement("div");
+   const subheadElement = document.createElement("h3");
+   
+   titleElement.innerHTML = item["songName"] || "";
+   artistElement.innerHTML = item["albumName"] ? `${item["artist"]} on "${item["albumName"]}"` : (item["artist"] || "");
+   subheadElement.innerHTML = 
+   (item["tempo"] ? `Tempo: ${item["tempo"]}` : "") + (item["difficulty"] ? ` / ${['Easiest', 'Easier', 'Easy', 'Not That Easy', 'Not Easy At All'][item["difficulty"] - 1]}` : "")
+   + (item["chordProgression"] ? ` / Chords: ${item["chordProgression"]}` : "");
+   document.getElementById("title").appendChild(titleElement);
+   document.getElementById("title").appendChild(artistElement);
+   document.getElementById("title").appendChild(subheadElement);
+
+   // Update description
+   const description = document.getElementById("description");
+   description.innerHTML = item["description"] ? `${item["description"]}` : "";
+
+   const nav = document.getElementById("data" + id);
+   document.querySelectorAll('li.data div.active').forEach((activeNavItem) => {
+       activeNavItem.classList.remove('active');
+   });
+   nav.children[0].classList.add('active');
+   nav.children[1].classList.add('active');
+   function playAudio() {
+
+      if (activeAudio) {
+        activeAudio.pause();
+      }
+      audio.play();
+      activeAudio = audio;
    }
+   playAudio();
+   const playButton = document.getElementById("playLick");
+   playButton.addEventListener("click", () => {
+   playAudio();
+   });
+}
 
 
 document.addEventListener('DOMContentLoaded', (event) => {
@@ -118,7 +125,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
          navigationDiv.style.display = "block";
          contentDiv.style.display = "block";
       }
-    }
+   }
    
     // Initial call to set visibility
     toggleVisibility();
